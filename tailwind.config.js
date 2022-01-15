c4cc30e6f6096bbca224c69629c1a1693143eb2e
Soham Nandi <sohamnandi77@gmail.com>
2022-01-15 05:38:55 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  purge: ["./src/components/**/*.js", "./pages/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [require("tailwindcss"), require("precss"), require("autoprefixer")],
+};
